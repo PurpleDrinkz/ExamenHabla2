95ef9ee688a7e48951297a69cb4be89a86c5dcb5
alumno <alumno@D3B09>
2018-03-22 10:15:10 -0700

    Add project files.

---
+++ Efectos/Delay.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Efectos
+{
+    class Delay : ISampleProvider
+    {
+        private ISampleProvider fuente;
+
+        public int offsetTiempoMS;
+        public float VolumenPerro;
+
+        List<float> muestras = new List<float>();
+
+        public Delay(ISampleProvider fuente)
+        {
+            this.fuente = fuente;
+            offsetTiempoMS = 600;
+            //50ms - 5000ms
+
+            VolumenPerro = 0.5f;
+            //0.0ms - 1.0ms
+    }
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+                return fuente.WaveFormat;
+            }
+        }
+
+
+        //Offset es el numero de muestras leídas hasta ahorita
+        public int Read(float[] buffer, int offset, int count)
+        {
+            //Calculo de tiempos
+            var read = fuente.Read(buffer, offset, count);
+            float tiempoTranscurrido =
+               (float) muestras.Count / (float)fuente.WaveFormat.SampleRate;
+            int muestrasTranscurridas = muestras.Count;
+            float tiempoTranscurridoMS = tiempoTranscurrido * 1000;
+            int numMuestrasOffsetTiempo = (int)
+                (((float)offsetTiempoMS / 1000.0f) * (float)fuente.WaveFormat.SampleRate);
+
+            //Añadir muestras a nuestro buffer
+            for (int i = 0; i < read; i++)
+            {
+                muestras.Add(buffer[i]);
+            }
+
+
+            //Modificar muestras
+            if ( tiempoTranscurridoMS > offsetTiempoMS)
+            {
+                for (int i = 0; i < read; i++)
+                {
+                    
+                    buffer[offset + i] +=
+                        muestras[muestrasTranscurridas +
+                        i-numMuestrasOffsetTiempo] *= VolumenPerro;
+
+
+
+
+                }
+
+
+            }
+
+            
+
+
+            return read;
+        }
+    }
+}
